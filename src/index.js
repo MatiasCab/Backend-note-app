97a1b0a8f4231e14db9cf844d0d97382970a374e
Walter Taño <tano.walter@hotmail.com>
2022-10-26 18:29:27 -0300

    todo mergeado

--- src/index.js
+++ src/index.js
@@ -13,8 +13,9 @@ const CURRENT_VERSION = 'v1';
 const CITIES_ENDPOINT = `/${CURRENT_VERSION}/places`;
 const NOTES_ENDPOINT = `/${CURRENT_VERSION}/notes`;
 
-const notes = [{ id: '1', color:'dark', title: 'Spiderman', placeId:"1", body:'texto de la note', temperature:'20', date:'2020/12/10'},
-{ id: '2', color:'dark', title: 'Titulo 2', placeId:"2", body:'texto de la note2', temperature:'30', date:'2020/11/10'}
+const notes = [
+  { id: '1', color:'dark', title: 'Spiderman', placeId:"1", body:'texto de la note', temperature:'20', date:'2020/12/10'},
+  { id: '2', color:'dark', title: 'Titulo 2', placeId:"2", body:'texto de la note2', temperature:'30', date:'2020/11/10'}
 ];
 
 // defining the Express app
@@ -34,21 +35,23 @@ app.use(morgan('combined'));
 
 // defining an endpoint to return all ads
 app.get(CITIES_ENDPOINT, async (req, res) => {
-    res.send(await getPlaces());
+  res.send(await getPlaces());
 });
 
-router.get(NOTES_ENDPOINT, function(req, res, next) {
+app.get(NOTES_ENDPOINT, function(req, res, next) {
   res.send(notes);
 });
 
-router.post(NOTES_ENDPOINT, async (req, res) =>{
+app.post(NOTES_ENDPOINT, async (req, res) =>{
   const newPost = req.body;
   notes.push(newPost);
-  res.status(200).send('Note added');
+  res.status(200).send('Note added successfully');
 })
 
-router.put(NOTES_ENDPOINT + '/:id', async (req, res) => {
+app.put(NOTES_ENDPOINT + '/:id', async (req, res) => {
   const updatedNote = req.body;
+  notes[req.params.id] = updatedNote;
+  res.status(200).send('Note updated successfully')
 })
 
 // starting the server
