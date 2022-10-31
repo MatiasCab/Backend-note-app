d01875281e9f0b274b545a8a5bc3fa9c402468d8
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2022-10-31 17:10:57 -0300

    Changed GET note by id

--- index.js
+++ index.js
@@ -9,16 +9,12 @@ const morgan = require('morgan');
 
 const { getPlaces, getPlace } = require('./src/cities/cities');
 const { connectToServer, getDb } = require('./src/server/db/conn');
+const { ObjectId } = require('mongodb');
 
 const CURRENT_VERSION = 'v1';
 const CITIES_ENDPOINT = `/${CURRENT_VERSION}/places`;
 const NOTES_ENDPOINT = `/${CURRENT_VERSION}/notes`;
 const NOTES_COLLECTION = 'notes';
-
-const notes = {
-    '1': { id: '1', clase:'bg-warning', titulo: 'Spiderman', ciudad:"1", cuerpo:'texto de la note', temperatura: '', date:'26/10/2022 19:23'},
-    '2': { id: '2', clase:'bg-success', titulo: 'Titulo 2', ciudad:"2", cuerpo:'texto de la note2', temperatura: '', date:'26/10/2022 20:20'}
-};
 
 // defining the Express app
 const app = express();
@@ -39,6 +35,7 @@ app.use(morgan('combined'));
 app.get(CITIES_ENDPOINT, async (req, res) => {
   getPlaces(res);
 });
+
 app.get(CITIES_ENDPOINT+"/:name", async (req, res) => {
   getPlace(res, req.params.name);
 });
@@ -59,11 +56,11 @@ app.get(NOTES_ENDPOINT, function(req, res, next) {
 app.get(NOTES_ENDPOINT + '/:id', (req, res) => {
   const id = req.params.id;
   getDb()
-  .collection(NOTES_COLLECTION).insertOne(newPost, function (err, _result) {
+  .collection(NOTES_COLLECTION).findOne({_id: ObjectId(id)}, function (err, result) {
     if (err) {
-      res.status(400).send({ error: true, message: `Error inserting note` });
+      res.status(400).send({ error: true, message: `Error geting note` });
     } else {
-      res.status(200).send({ id: newPost.id, success: true, message: 'Note added successfully' })
+      res.status(200).send(result)
     }
   });
 
@@ -104,7 +101,6 @@ app.put(NOTES_ENDPOINT + '/:id', async (req, res) => {
       res.status(400).send(`Cant update` + noteId);
     } else {
       res.status(200).send({ id: req.params.id, success: true, message: 'Note updated successfully' })
-      console.log("Document with id: " + req.params.id + ' was updated');
     }
   });
 
